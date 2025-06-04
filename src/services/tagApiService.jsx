c33ddadb32f8bd5cce8daea608037b0fe881b890
mateo1272001 <mateovazquezreyes@gmail.com>
2025-06-03 22:01:11 -0300

    changing url to deploy on test

--- src/services/tagApiService.jsx
+++ src/services/tagApiService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/tag'; // Reemplaza con la URL real
+const BASE_URL = 'http://proaudio-test.c6zfg2tqcsfh.us-east-1.rds.amazonaws.com/tag'; // Reemplaza con la URL real
 
 export const getAllTags = async () => {
   const response = await axios.get(BASE_URL + '/all');
