5cb7435c35bdbaa4b6b3a86c2d12f9617d6b4d2a
MarcosASandes <agustin0818sandes@gmail.com>
2025-08-03 18:44:57 -0300

    feat(auth): add BackgroundParticles for background login

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "../../styles/auth/login.module.css";
 import LoginForm from "../../components/auth/LoginForm";
+import BackgroundParticles from "../../components/global/BackgroundParticles";
 
 export default function LoginPage() {
   return (
     <div className={styles.loginPage}>
+        <BackgroundParticles />
       <LoginForm />
     </div>
   );
