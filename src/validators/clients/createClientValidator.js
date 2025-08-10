2934ffc39be526a5785e62616ec0ed562141bcd9
MarcosASandes <agustin0818sandes@gmail.com>
2025-08-09 22:02:08 -0300

    feat(client): add changes to validations in create and update client validators

--- src/validators/clients/createClientValidator.js
+++ src/validators/clients/createClientValidator.js
@@ -1,17 +1,28 @@
 import * as yup from "yup";
 
 const createClientValidator = yup.object().shape({
-  name: yup.string().required("Este campo es obligatorio."),
+  name: yup
+    .string()
+    .max(20, "El nombre debe tener como máximo 20 caracteres")
+    .required("Este campo es obligatorio."),
   phone: yup
     .string()
+    .max(12, "La longitud de caracteres del teléfono debe ser 12")
     .required("El teléfono es obligatorio.")
     .matches(/^[0-9+()\-\s]*$/, "Formato de teléfono inválido."),
   email: yup
     .string()
+    .max(50, "El email debe tener una longitud menor o igual a 50 caracteres")
     .required("El correo es obligatorio.")
     .email("Debe ser un correo válido."),
-  address: yup.string().required("La dirección es obligatoria."),
-  details: yup.string().notRequired(),
+  address: yup
+    .string()
+    .max(100, "La dirección no puede tener más de 100 caracteres")
+    .required("La dirección es obligatoria."),
+  details: yup
+    .string()
+    .max(150, "El detalle no puede tener más de 150 caracteres")
+    .notRequired(),
 });
 
 export default createClientValidator;
--- src/validators/clients/updateClientValidator.js
+++ src/validators/clients/updateClientValidator.js
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export const updateClientValidator = yup.object().shape({
-  name: yup
+  /*name: yup
     .string()
     .required("El nombre es obligatorio")
     .max(100, "Máximo 100 caracteres"),
@@ -22,5 +22,27 @@ export const updateClientValidator = yup.object().shape({
   details: yup
     .string()
     .nullable()
-    .max(500, "Máximo 500 caracteres"),
+    .max(500, "Máximo 500 caracteres"),*/
+  name: yup
+    .string()
+    .max(20, "El nombre debe tener como máximo 20 caracteres")
+    .required("Este campo es obligatorio."),
+  phone: yup
+    .string()
+    .max(12, "La longitud de caracteres del teléfono debe ser 12")
+    .required("El teléfono es obligatorio.")
+    .matches(/^[0-9+()\-\s]*$/, "Formato de teléfono inválido."),
+  email: yup
+    .string()
+    .max(50, "El email debe tener una longitud menor o igual a 50 caracteres")
+    .required("El correo es obligatorio.")
+    .email("Debe ser un correo válido."),
+  address: yup
+    .string()
+    .max(100, "La dirección no puede tener más de 100 caracteres")
+    .required("La dirección es obligatoria."),
+  details: yup
+    .string()
+    .max(150, "El detalle no puede tener más de 150 caracteres")
+    .notRequired(),
 });
