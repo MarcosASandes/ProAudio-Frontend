1855dfb20c58c67be9959e726f0686936352bc27
MarcosASandes <agustin0818sandes@gmail.com>
2025-08-20 21:18:21 -0300

    refactor(event): create embedded event form stylized

--- src/pages/events/CreateEmbeddedEventPage.jsx
+++ src/pages/events/CreateEmbeddedEventPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import CreateEmbeddedEventForm from "../../components/events/CreateEmbeddedEventForm";
 import stylesSectionContainer from "../../styles/generic/sectionContainer.module.css";
 import stylesContainerSpace from "../../styles/generic/containerSpace.module.css";
@@ -8,6 +8,26 @@ import stylesContainerSpace from "../../styles/generic/containerSpace.module.css
 const CreateEmbeddedEventPage = () => {
   return (
     <div className={`${stylesSectionContainer.sectionContainerDark} ${stylesContainerSpace.mainSection}`}>
+      <CreateEmbeddedEventForm />
+    </div>
+  );
+};
+
+export default CreateEmbeddedEventPage;*/
+
+
+
+/*-------------------------------------------- */
+
+
+
+import React from "react";
+import stylesScrollableContainer from "../../styles/generic/scrollableContainer.module.css";
+import CreateEmbeddedEventForm from "../../components/events/CreateEmbeddedEventForm";
+
+const CreateEmbeddedEventPage = () => {
+  return (
+    <div className={stylesScrollableContainer.container}>
       <CreateEmbeddedEventForm />
     </div>
   );
