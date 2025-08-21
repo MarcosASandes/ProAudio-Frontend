abb7394e2da441085f3cc1ba5de0624c6dbf80ff
MarcosASandes <agustin0818sandes@gmail.com>
2025-08-21 18:15:43 -0300

    refactor(project): client embedded form stylized

--- src/pages/clients/CreateEmbeddedClientPage.jsx
+++ src/pages/clients/CreateEmbeddedClientPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import stylesSectionContainer from "../../styles/generic/sectionContainer.module.css";
 import stylesContainerSpace from "../../styles/generic/containerSpace.module.css";
 import CreateEmbeddedClientForm from "../../components/clients/CreateEmbeddedClientForm";
@@ -8,6 +8,24 @@ import CreateEmbeddedClientForm from "../../components/clients/CreateEmbeddedCli
 const CreateEmbeddedClientPage = () => {
   return (
     <div className={`${stylesSectionContainer.sectionContainerDark} ${stylesContainerSpace.mainSection}`}>
+      <CreateEmbeddedClientForm />
+    </div>
+  );
+};
+
+export default CreateEmbeddedClientPage;*/
+
+
+/*--------------------------------------------------- */
+
+
+import React from "react";
+import stylesScrollableContainer from "../../styles/generic/scrollableContainer.module.css";
+import CreateEmbeddedClientForm from "../../components/clients/CreateEmbeddedClientForm";
+
+const CreateEmbeddedClientPage = () => {
+  return (
+    <div className={stylesScrollableContainer.container}>
       <CreateEmbeddedClientForm />
     </div>
   );
