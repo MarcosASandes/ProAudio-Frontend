421dc784182f56ec6fe804635f5bbc460e072be5
MarcosASandes <agustin0818sandes@gmail.com>
2025-08-24 03:25:12 -0300

    fix(notification): change interval

--- src/hooks/notifications/useGetRecentNotifications.jsx
+++ src/hooks/notifications/useGetRecentNotifications.jsx
@@ -146,7 +146,7 @@ const useGetRecentNotifications = (
     };
   }, [dispatch, page, size, type, is_completed, name]);
 
-  /*useEffect(() => {
+  useEffect(() => {
     // Llamada inicial
     fetchRef.current();
 
@@ -155,18 +155,7 @@ const useGetRecentNotifications = (
       if (fetchRef.current) {
         fetchRef.current();
       }
-    }, intervalMinutes * 60 * 1000);*/
-
-    useEffect(() => {
-    // Llamada inicial
-    fetchRef.current();
-
-    // Llamadas periódicas
-    const intervalId = setInterval(() => {
-      if (fetchRef.current) {
-        fetchRef.current();
-      }
-    }, 20000);
+    }, intervalMinutes * 60 * 1000);
 
     return () => clearInterval(intervalId);
   }, [intervalMinutes]);
