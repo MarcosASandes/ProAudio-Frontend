7b666342017d4a069e5784ab9e1b202bdfd6fdb0
MarcosASandes <agustin0818sandes@gmail.com>
2025-08-31 18:09:50 -0300

    feat(project): add tooltip in project view to explain de row colors

--- src/components/projects/ProjectView.jsx
+++ src/components/projects/ProjectView.jsx
@@ -18,7 +18,7 @@
  * @returns {JSX.Element} Vista completa con filtros, tabla y paginación de proyectos.
  */
 
-import React, { useState, useEffect } from "react";
+/*import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import ProjectFilter from "./ProjectFilter";
 import ProjectsTable from "./ProjectsTable";
@@ -75,9 +75,139 @@ const ProjectView = () => {
 
   return (
     <div className={styles.container}>
-      {/* Header */}
+     
       <div className={styles.header}>
         <h2 className={styles.title}>Proyectos</h2>
+        <div className={styles.actions}>
+          <button className={styles.calendarButton} onClick={handleGoToEvents}>
+            <Calendar size={18} />
+            <span className={styles.calendarText}>Ver eventos</span>
+          </button>
+
+          <button className={styles.createButton} onClick={handleGoToCreate}>
+            Crear proyecto
+          </button>
+        </div>
+      </div>
+
+      <ProjectFilter
+        searchTerm={searchTerm}
+        onSearchChange={setSearchTerm}
+        sortBy={sortBy}
+        onSortByChange={setSortBy}
+        direction={direction}
+        onDirectionChange={setDirection}
+        filterStatus={filterStatus}
+        onFilterStatusChange={setFilterStatus}
+        filterPaymentStatus={filterPaymentStatus}
+        onFilterPaymentStatusChange={setFilterPaymentStatus}
+      />
+
+      {loading && <p>Cargando proyectos...</p>}
+      {error && <p className="text-danger">{error}</p>}
+      {!loading && !error && (
+        <div className={styles.tableWrapper}>
+          <ProjectsTable projects={projects} />
+        </div>
+      )}
+
+      <div className={styles.paginationWrapper}>
+        <Pagination pageable={pageable} onPageChange={setCurrentPage} />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectView;*/
+
+/*------------------------------ */
+
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import ProjectFilter from "./ProjectFilter";
+import ProjectsTable from "./ProjectsTable";
+import useGetAllProjects from "../../hooks/projects/useGetAllProjects";
+import {
+  selectProjects,
+  selectProjectPageable,
+  selectProjectsLoading,
+  selectProjectsError,
+} from "../../features/projects/ProjectSelector";
+import { useNavigate } from "react-router-dom";
+import styles from "../../styles/projects/projectView.module.css";
+import { Calendar, Info } from "lucide-react";
+import Pagination from "../global/Pagination";
+
+const ProjectView = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+
+  const [sortBy, setSortBy] = useState("start_date");
+  const [direction, setDirection] = useState("desc");
+  const [filterStatus, setFilterStatus] = useState([]);
+  const [filterPaymentStatus, setFilterPaymentStatus] = useState("");
+
+  const navigate = useNavigate();
+
+  useGetAllProjects(
+    currentPage,
+    pageSize,
+    sortBy,
+    direction,
+    filterStatus,
+    filterPaymentStatus,
+    searchTerm
+  );
+
+  const projects = useSelector(selectProjects);
+  const pageable = useSelector(selectProjectPageable);
+  const loading = useSelector(selectProjectsLoading);
+  const error = useSelector(selectProjectsError);
+
+  const handleGoToCreate = () => {
+    navigate("/project/create");
+  };
+
+  const handleGoToEvents = () => {
+    navigate("/events");
+  };
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [searchTerm, sortBy, direction, filterStatus, filterPaymentStatus]);
+
+  return (
+    <div className={styles.container}>
+      {/* Header */}
+      <div className={styles.header}>
+        <div className={styles.labelWithIcon}>
+          <h2 className={styles.title}>Proyectos</h2>
+          <div className={styles.infoIconWrap} aria-hidden="true">
+            <Info size={15} />
+            <div className={`${styles.tooltip} ${styles.tooltipRight}`}>
+              Los colores de la fila representan proyectos en distintos estados.
+              <div>
+                <span className={styles.tooltipRedRowKey}>Rojo:</span> proyecto
+                en curso.
+              </div>
+              <div>
+                <span className={styles.tooltipYellowRowKey}>Amarillo:</span>{" "}
+                proyecto en preparación.
+              </div>
+              <div>
+                <span className={styles.tooltipGreenRowKey}>Verde:</span>{" "}
+                proyecto finalizado.
+              </div>
+              <div>
+                <span className={styles.tooltipDarkBlueRowKey}>
+                  Azul oscuro:
+                </span>{" "}
+                proyecto descartado.
+              </div>
+            </div>
+          </div>
+        </div>
         <div className={styles.actions}>
           <button className={styles.calendarButton} onClick={handleGoToEvents}>
             <Calendar size={18} />
